712de755244480d22faeaeaff1eac33f6e959978
grosser <grosser.michael@gmail.com>
2009-09-01 21:16:29 +0200

    remove openssl from dependencies, cant be installed even if presen (not a rubygem...)

--- Rakefile.rb
+++ Rakefile.rb
@@ -14,7 +14,6 @@ begin
     gem.email = "grosser.michael@gmail.com"
     gem.homepage = "http://github.com/grosser/#{project_name}"
     gem.authors = ["Michael Grosser"]
-    gem.add_dependency ['openssl']
   end
 rescue LoadError
   puts "Jeweler, or one of its dependencies, is not available. Install it with: sudo gem install technicalpickles-jeweler -s http://gems.github.com"
